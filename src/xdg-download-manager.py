d3195f7e161cc5d6c3a86bbf92af7dde3b68f49a
Ikem Krueger <ikem.krueger@googlemail.com>
2018-07-28 14:51:54 +0200

    Add switch for notifcation

--- src/xdg-download-manager.py
+++ src/xdg-download-manager.py
@@ -51,7 +51,7 @@ def get_media_type(filename):
 
     return media_type
 
-def move_file(filename, dry_run=False):
+def move_file(filename, dry_run=False, notifcation=True):
     media_type = get_media_type(filename)
     xdg_dest_dir = get_xdg_folder(xdg_folder[media_type])
 
@@ -63,7 +63,8 @@ def move_file(filename, dry_run=False):
         if not dry_run:
             subprocess.call(["mv", "-f", filename, xdg_dest_dir])
 
-        subprocess.call(["notify-send", "-i", "media-floppy", "Download finished!", filename])
+        if notification:
+            subprocess.call(["notify-send", "-i", "media-floppy", "Download finished!", filename])
 
 def action(action, filename, *args):
     print("action: {}".format(action.__name__))
